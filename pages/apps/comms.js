35ac42aea109ad95df425c86a9e5f861f95ba95b
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-08-02 10:22:33 +0200

    feat: application pages

---
+++ pages/apps/comms.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import getConfig from 'next/config'
+
+const comms = () => {
+  const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
+    return (
+        <div>
+      <Head>
+      <title>Dispatch | {`${publicRuntimeConfig.serverName}`}</title>
+        <meta name="description" content="Apply for dispatch on our awesome website by clicking on me!" />
+        <link rel="icon" href="/favicon.ico" />
+        
+      </Head>
+<div className="m-0 h-half z-40 overflow-hidden">
+<iframe className="absolute h-full left-0 right-0 bottom-0 top-0" id="typeform-full" width="100%" height="100%" frameBorder="0" allow="camera; microphone; autoplay; encrypted-media;" src={`${publicRuntimeConfig.lspdApp}?typeform-medium=embed-snippet`}></iframe>  
+</div>
+       </div>
+    );
+}
+ 
+export default comms;
---
+++ pages/apps/lspd.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import getConfig from 'next/config'
+
+const lspd = () => {
+  const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
+    return (
+        <div>
+      <Head>
+      <title>LSPD | {`${publicRuntimeConfig.serverName}`}</title>
+        <meta name="description" content="Apply for lspd on our awesome website by clicking on me!" />
+        <link rel="icon" href="/favicon.ico" />
+        
+      </Head>
+<div className="m-0 h-half z-40 overflow-hidden">
+<iframe className="absolute h-full left-0 right-0 bottom-0 top-0" id="typeform-full" width="100%" height="100%" frameBorder="0" allow="camera; microphone; autoplay; encrypted-media;" src={`${publicRuntimeConfig.lspdApp}?typeform-medium=embed-snippet`}></iframe>  
+</div>
+       </div>
+    );
+}
+ 
+export default lspd;
