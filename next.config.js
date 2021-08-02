b53783f78be8a9067b466016325e4bb5e2e02a29
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-08-02 09:33:08 +0200

    feat: add config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
 module.exports = {
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: 'secret',
+    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: '/static',
+    serverName: "JoeMama",
+    fullServerName: "Joe Mama RP",
+    discordLink: "https://discord.gg/YXa8AUW7Sf",
+    fivemServerId: "vjarme",
+    facebook: "https://facebook.com",
+    twitter: "https://twitter.com",
+    instagram: "https://instagram.com",
+    lspdApp: "https://form.typeform.com/to/d3cL14qy",
+    bcsoApp: "",
+    civApp: "",
+    commsApp: "",
+    devApp: "",
+    staffApp: "",
+    sahpApp: "",
+    safrApp: "",
+  },
   reactStrictMode: true,  
   images: {
-    domains: ['images.pexels.com', 'hamy.iodine.gg', 'i.iodine.gg', 'c.iodine.gg', 'images.unsplash.com', 'dummyimage.com','hamy.fileglass.com', 'i.file.glass', 'c.file.glass'],
+    domains: ['hamy.fileglass.com'],
   },
 }
