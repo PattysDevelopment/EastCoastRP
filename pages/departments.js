8019e824ef63cf56fa26fe167086f31ed3f24850
Patrick Calcote <88861117+PattysDevelopment@users.noreply.github.com>
2022-05-17 11:23:47 -0500

    Update departments.js

--- pages/departments.js
+++ pages/departments.js
@@ -19,9 +19,9 @@ export default function Departments() {
               <div className="flex flex-wrap mb-12 text-left">
                 <div className="w-full p-6 mx-auto lg:w-1/3">
                   <div className="p-4 lg:p-8 rounded-xl ">
-                    <Image className="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl" src="https://i.file.glass/16hej.webp" width="720" height="400" alt="a image of the los santos police department standing shoulder to shoulder"/>
-                    <h1 className="mx-auto mb-8 text-2xl font-semibold leading-none tracking-tighter transition duration-300 ease-in-out dark:text-white title-font"> Los Santos Police Department </h1>
-                    <p className="mx-auto text-base font-medium leading-relaxed transition duration-300 ease-in-out dark:text-gray-100 "> The Police Department’s goal is to protect and maintain Los Santos high quality of life and prevent crime. The department is responsible for enhancing overall community safety and safeguarding the rights of individuals. The Police Department is organized into specialized units and teams, staffed by both full-time, and volunteer officers to manage calls for service and facilitate problem-solving throughout the community.  </p>
+                    <Image className="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl" src="https://cdn.discordapp.com/attachments/969777717685915669/976157955672125520/unknown.png" width="720" height="400" alt="a image of the los santos police department standing shoulder to shoulder"/>
+                    <h1 className="mx-auto mb-8 text-2xl font-semibold leading-none tracking-tighter transition duration-300 ease-in-out dark:text-white title-font"> Florida Highway Patrol </h1>
+                    <p className="mx-auto text-base font-medium leading-relaxed transition duration-300 ease-in-out dark:text-gray-100 ">Florida Highway Patrol’s goal is to protect and maintain Los Santos high quality of life and prevent crime. The department is responsible for enhancing overall community safety and safeguarding the rights of individuals. The Police Department is organized into specialized units and teams, staffed by both full-time, and volunteer officers to manage calls for service and facilitate problem-solving throughout the community.  </p>
                     <Link href="apps/lspd" title="apply now" passHref>
                       <div>
                       <button className="inline-flex items-center mt-4 font-semibold text-blue-600 lg:mb-0 hover:text-black ">
